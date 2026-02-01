c6f8d62bdaaf5f354a9f16866869a2586359def4
Silici8m <vik.forc38@gmail.com>
2026-02-01 14:53:46 +0000

    Eviter le crash complet

--- build.sh
+++ build.sh
@@ -7,4 +7,5 @@ colcon build \
         --merge-install \
         --symlink-install \
         --cmake-args "-DCMAKE_BUILD_TYPE=$BUILD_TYPE" "-DCMAKE_EXPORT_COMPILE_COMMANDS=On" \
-        -Wall -Wextra -Wpedantic
+        -Wall -Wextra -Wpedantic \
+        --parallel-workers 4
